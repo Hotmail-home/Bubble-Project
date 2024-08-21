807346ed36bf02f1d41885b97cc6572d8ad8b466
Egypt <Egypt@DESKTOP-LL3436A>
2024-08-22 00:11:02 +0300

    Add project files.

---
+++ BubblePanel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public class BubblePanel : Panel
+{
+    public BubblePanel()
+    {
+        // To avoid flickering
+        this.DoubleBuffered = true;
+    }
+
+    private Color bc = Color.FromArgb(128, Color.Aqua);
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        // Set smoothing for better quality
+        e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+        // Define the bubble shape
+        int borderRadius = 50; // Adjust for roundness
+        Rectangle bubbleBounds = new Rectangle(10, 10, this.Width - 20, this.Height - 20);
+
+        // Create a shadow effect
+        Rectangle shadowBounds = new Rectangle(bubbleBounds.X + 10, bubbleBounds.Y + 10, bubbleBounds.Width, bubbleBounds.Height);
+        using (GraphicsPath shadowPath = new GraphicsPath())
+        {
+            shadowPath.AddEllipse(shadowBounds);
+            using (PathGradientBrush shadowBrush = new PathGradientBrush(shadowPath))
+            {
+                shadowBrush.CenterColor = Color.FromArgb(60, Color.Black); // Transparent shadow
+                shadowBrush.SurroundColors = new[] { Color.Transparent };
+                e.Graphics.FillPath(shadowBrush, shadowPath);
+            }
+        }
+
+        // Create the bubble shape with base color
+        using (GraphicsPath bubblePath = new GraphicsPath())
+        {
+            bubblePath.AddEllipse(bubbleBounds);
+
+            using (LinearGradientBrush bubbleBrush = new LinearGradientBrush(bubbleBounds, Color.FromArgb(bc.R / 2, bc.G / 2, bc.B / 2), bc, LinearGradientMode.Vertical))
+            {
+                e.Graphics.FillPath(bubbleBrush, bubblePath);
+            }
+
+            // Draw the bubble border
+            using (Pen pen = new Pen(Color.FromArgb(25, Color.Black), 3))
+            {
+                e.Graphics.DrawPath(pen, bubblePath);
+            }
+        }
+
+        // Add glossy highlight (water reflection effect)
+        Rectangle highlightBounds = new Rectangle(bubbleBounds.X + 15, bubbleBounds.Y + 15, bubbleBounds.Width / 2, bubbleBounds.Height / 2);
+        using (GraphicsPath highlightPath = new GraphicsPath())
+        {
+            highlightPath.AddEllipse(highlightBounds);
+            using (PathGradientBrush highlightBrush = new PathGradientBrush(highlightPath))
+            {
+                highlightBrush.CenterColor = Color.FromArgb(180, Color.White); // Semi-transparent white
+                highlightBrush.SurroundColors = new[] { Color.Transparent };
+                e.Graphics.FillPath(highlightBrush, highlightPath);
+            }
+        }
+
+        // Add rainbow layers on top
+        AddRainbowLayers(e.Graphics, bubbleBounds);
+    }
+
+    private void AddRainbowLayers(Graphics g, Rectangle bounds)
+    {
+        // Define the rainbow colors
+        Color[] rainbowColors = new Color[]
+        {
+            Color.Red,
+            Color.Orange,
+            Color.Yellow,
+            Color.Green,
+            Color.Blue,
+            Color.Indigo,
+            Color.Violet
+        };
+
+        // Loop through the rainbow colors and draw layers
+        int layerOffset = 10; // Increase the offset to reduce the number of layers
+        foreach (var color in rainbowColors)
+        {
+            Rectangle layerBounds = new Rectangle(bounds.X + layerOffset, bounds.Y + layerOffset, bounds.Width - layerOffset * 2, bounds.Height - layerOffset * 2);
+
+            // Avoid drawing if the bounds are too small
+            if (layerBounds.Width <= 0 || layerBounds.Height <= 0)
+                break;
+
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddEllipse(layerBounds);
+                using (PathGradientBrush brush = new PathGradientBrush(path))
+                {
+                    brush.CenterColor = Color.FromArgb(100, color); // Semi-transparent color
+                    brush.SurroundColors = new[] { Color.Transparent };
+                    g.FillPath(brush, path);
+                }
+            }
+
+            layerOffset += 15; // Adjust layer thickness
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Bubble_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bubblePanel2 = new BubblePanel();
+            this.bubblePanel1 = new BubblePanel();
+            this.SuspendLayout();
+            // 
+            // bubblePanel2
+            // 
+            this.bubblePanel2.Location = new System.Drawing.Point(410, 159);
+            this.bubblePanel2.Name = "bubblePanel2";
+            this.bubblePanel2.Size = new System.Drawing.Size(248, 245);
+            this.bubblePanel2.TabIndex = 1;
+            // 
+            // bubblePanel1
+            // 
+            this.bubblePanel1.Location = new System.Drawing.Point(31, 1);
+            this.bubblePanel1.Name = "bubblePanel1";
+            this.bubblePanel1.Size = new System.Drawing.Size(313, 330);
+            this.bubblePanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bubblePanel2);
+            this.Controls.Add(this.bubblePanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BubblePanel bubblePanel1;
+        private BubblePanel bubblePanel2;
+    }
+}
+
